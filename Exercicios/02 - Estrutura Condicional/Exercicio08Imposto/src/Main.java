581a35d3e3c4c7e5115cdf6c7869d27b671aab17
WesleyJSToledo <wesleyjstoledo@gmail.com>
2022-10-12 15:05:47 -0300

    Atualizacao de Arquivos

---
+++ Exercicios/02 - Estrutura Condicional/Exercicio08Imposto/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner lerSalario = new Scanner(System.in);
+		
+		double salario;
+		double imposto;
+		
+		System.out.print("Salario R$");
+		salario = lerSalario.nextDouble();
+		
+		if (salario <= 2000.0) {
+			imposto = 0.0;
+		}
+		else if (salario <= 3000.0) {
+			imposto = (salario - 2000.0) * 0.08;
+		}
+		else if (salario <= 4500.0) {
+			imposto = (salario - 3000.0) * 0.18 + 1000.0 * 0.08;
+		}
+		else {
+			imposto = (salario - 4500.0) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+		}
+
+		if (imposto == 0.0) {
+			System.out.println("Isento");
+		}
+		else {
+			System.out.printf("R$ %.2f%n", imposto);
+		}
+	}
+
+}
