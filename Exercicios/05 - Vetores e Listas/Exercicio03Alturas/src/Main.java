f817ffc727cf04fe24bd1521d4aff14703a3d6c3
WesleyJSToledo <wesleyjstoledo@gmail.com>
2023-01-09 23:01:51 -0300

    Exercicio 03 Vetores e Listas

---
+++ Exercicios/05 - Vetores e Listas/Exercicio03Alturas/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int quantasPessoas;
+		float somaAtura = 0, mediaAltura;
+
+		System.out.println("Quantas Pessoas Irá Cadastrar? ");
+		quantasPessoas = sc.nextInt();
+
+		Pessoas[] pessoas = new Pessoas[quantasPessoas];
+
+		for (int i = 0; i < pessoas.length; i++) {
+			pessoas[i] = new Pessoas();
+			
+			pessoas[i].instanciarValores();
+			
+			somaAtura += pessoas[i].getAltura();
+		}
+
+		mediaAltura = somaAtura / quantasPessoas;
+		System.out.println("Media de Altura: " + mediaAltura);
+
+		for (Pessoas pessoa : pessoas) {
+			if (pessoa.getIdade() <= 16) {
+				System.out.println(pessoa.getNome());
+			}
+		}
+		sc.close();
+	}
+
+}
