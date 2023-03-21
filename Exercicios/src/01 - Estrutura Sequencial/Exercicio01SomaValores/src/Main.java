921cba5e248a390240bebcb046e3c00ff148d7d7
Wesley José dos Santos Toledo <90799825+WesleyJSToledo@users.noreply.github.com>
2023-03-21 16:34:40 -0300

    Organizando Pastas

---
+++ Exercicios/src/01 - Estrutura Sequencial/Exercicio01SomaValores/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner lerNumeros = new Scanner(System.in);
+		
+		int primeiroNumero, segundoNumero, soma;
+		
+		System.out.println("Digite o Primeiro Valor: ");
+		primeiroNumero = lerNumeros.nextInt();
+		
+		System.out.println("Digite o Segundo Valor: ");
+		segundoNumero = lerNumeros.nextInt();
+		
+		soma = primeiroNumero + segundoNumero;
+		
+		System.out.println("Soma = " +soma);
+
+		lerNumeros.close();
+	}
+}
