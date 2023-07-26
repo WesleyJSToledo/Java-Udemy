5eb40fcd9c62cfc5ed174c1c23cd7799ea26d3bd
WesleyJSToledo <wesleyjstoledo@gmail.com>
2023-07-26 09:12:50 -0300

    JDBC

---
+++ src/jdbc/transactions/application/Program.java
@@ -0,0 +1,42 @@
+package jdbc.transactions.application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.jdbc.exceptions.SQLExceptionsMapping;
+
+import jdbc.db.DB;
+import jdbc.db.DBException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		Statement st = null;
+		
+		try {
+			connection = DB.getConnection();
+			connection.setAutoCommit(false);
+			st = connection.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = BaseSalary + 2000 WHERE DepartmentId = 1");
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = BaseSalary + 3000 WHERE DepartmentId = 2");
+			
+			
+			connection.commit();
+			
+			System.out.println("Rows 1: " + rows1);
+			System.out.println("Rows 2: " + rows2);
+			
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+				throw new DBException(e.getMessage());
+			} catch (SQLException ex) {
+				throw new DBException("Error trying to rollback! Caused by: "+ ex.getMessage());
+			}
+		}
+	}
+
+}
