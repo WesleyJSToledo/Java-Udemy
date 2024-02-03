bbd4d4b63294aa06fde4e0ed5e6e218d8de6e74d
WesleyJSToledo <wesleyjstoledo@gmail.com>
2024-02-03 00:05:42 -0300

    Adicionando Maven

--- src/stream/products_csv/Main.java
+++ src/stream/products_csv/Main.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import stream.products_csv.entities.Product;
